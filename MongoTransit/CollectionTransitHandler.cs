fdb38ba803c442d23301554cf19d7cc8d7070618
Alexey Maltsev <axel.user.perm@gmail.com>
2021-02-28 20:10:58 +0300

    Implemented batching

--- MongoTransit/CollectionTransitHandler.cs
+++ MongoTransit/CollectionTransitHandler.cs
@@ -1,52 +1,118 @@
 using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace MongoTransit
 {
-    public static class CollectionTransitHandler
+    public class CollectionTransitHandler
     {
-        public static async Task RunAsync(CollectionTransitOptions options, CancellationToken token)
+        private readonly CollectionTransitOptions _options;
+        private readonly IMongoCollection<BsonDocument> _fromCollection;
+        private readonly IMongoCollection<BsonDocument> _toCollection;
+
+        public CollectionTransitHandler(CollectionTransitOptions options)
         {
-            var fromClient = new MongoClient(options.FromConnectionString);
+            _options = options;
             
             // TODO check DB for existence
-            var fromDb = fromClient.GetDatabase(options.Database);
-
             // TODO check collection for existence
-            var fromCollection = fromDb.GetCollection<BsonDocument>(options.Collection);
-            
-            var toClient = new MongoClient(options.FromConnectionString);
-            
+            _fromCollection = new MongoClient(options.FromConnectionString).GetDatabase(options.Database)
+                .GetCollection<BsonDocument>(options.Collection);
+
             // TODO check DB for existence
-            var toDb = fromClient.GetDatabase(options.Database);
-
             // TODO check collection for existence
-            var toCollection = fromDb.GetCollection<BsonDocument>(options.Collection);
-
-            var checkpointField = options.IterativeTransferOptions.Field;
-            var lastCheckpoint = options.IterativeTransferOptions?.ForcedCheckpoint ??
-                                 await FindCheckpointAsync(toCollection, checkpointField, token);
+            _toCollection = new MongoClient(options.ToConnectionString).GetDatabase(options.Database)
+                .GetCollection<BsonDocument>(options.Collection);
+        }
+        
+        public async Task TransitAsync(CancellationToken token)
+        {
+            var transitChannel = Channel.CreateBounded<(int count, WriteModel<BsonDocument>[] batch)>(_options.Workers);
             
-            // TODO handle if last checkpoint is null
-            var filter = new BsonDocument(checkpointField, new BsonDocument("$gte", lastCheckpoint));
-            var fromCursor = await fromCollection.FindAsync(filter, cancellationToken: token);
-
-            await fromCursor.ForEachAsync(async document =>
+            IAsyncCursor<BsonDocument> fromCursor = await CreateReadingCursorAsync(token);
+            
+            var insertionWorkers = new List<Task>(_options.Workers);
+            for (var workerN = 0; workerN < _options.Workers; workerN++)
             {
-                var filter = new BsonDocument();
-                foreach (var upsertField in options.UpsertFields)
+                insertionWorkers[workerN] = RunWorker(transitChannel.Reader, token);
+            }
+            
+            await ReadDocumentsAsync(_options.BatchSize, _options.UpsertFields, fromCursor, transitChannel.Writer, token);
+
+            await Task.WhenAll(insertionWorkers);
+        }
+
+        private static async Task ReadDocumentsAsync(int batchSize, string[] upsertFields, IAsyncCursor<BsonDocument> documentsReader,
+            ChannelWriter<(int count, WriteModel<BsonDocument>[] batch)> batchWriter, CancellationToken token)
+        {
+            WriteModel<BsonDocument>[] batch = ArrayPool<WriteModel<BsonDocument>>.Shared.Rent(batchSize);
+            var count = 0;
+            await documentsReader.ForEachAsync(async document =>
+            {
+                if (count < batchSize)
                 {
-                    filter[upsertField] = document[upsertField];
+                    var filter = new BsonDocument();
+                    foreach (var field in upsertFields)
+                    {
+                        filter[field] = document[field];
+                    }
+
+                    batch[count] = new ReplaceOneModel<BsonDocument>(filter, document)
+                    {
+                        IsUpsert = true
+                    };
+                    count++;
                 }
-                await toCollection.UpdateOneAsync(filter, document, new UpdateOptions
+                else
                 {
-                    IsUpsert = true,
+                    await batchWriter.WriteAsync((count, batch), token);
+                    count = 0;
+                    batch = ArrayPool<WriteModel<BsonDocument>>.Shared.Rent(batchSize);
+                }
+            }, token);
+
+            // Handle case when cursor is finished, but there are some documents less than batch.
+            if (count > 0)
+            {
+                // Flush remaining documents
+                await batchWriter.WriteAsync((count, batch), token);
+            }
+            batchWriter.Complete();
+        }
+
+        private async Task RunWorker(ChannelReader<(int count, WriteModel<BsonDocument>[] batch)> batchReader, CancellationToken token)
+        {
+            await foreach (var (count, batch) in batchReader.ReadAllAsync(token))
+            {
+                await _toCollection.BulkWriteAsync(batch.Take(count), new BulkWriteOptions
+                {
+                    IsOrdered = false,
                     BypassDocumentValidation = true
                 }, token);
-            }, token);
+                
+                ArrayPool<WriteModel<BsonDocument>>.Shared.Return(batch);
+            }
+        }
+
+        private async Task<IAsyncCursor<BsonDocument>> CreateReadingCursorAsync(CancellationToken token)
+        {
+            if (_options.IterativeTransferOptions == null)
+                // Full collection transition
+                return await _fromCollection.FindAsync(null, cancellationToken: token);
+            
+            var checkpointField = _options.IterativeTransferOptions.Field;
+            var lastCheckpoint = _options.IterativeTransferOptions?.ForcedCheckpoint ??
+                                 await FindCheckpointAsync(_toCollection, checkpointField, token);
+
+            // TODO handle if last checkpoint is null
+            var filter = new BsonDocument(checkpointField, new BsonDocument("$gte", lastCheckpoint));
+            return await _fromCollection.FindAsync(filter, cancellationToken: token);
         }
 
         private static async Task<DateTime?> FindCheckpointAsync(IMongoCollection<BsonDocument> collection, string checkpointField, CancellationToken token)
--- MongoTransit/TransitRunner.cs
+++ MongoTransit/TransitRunner.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,16 +16,21 @@ namespace MongoTransit
 
         public async Task RunAsync(CancellationToken token)
         {
+            var options = ConfigurationReader.Read(_configPath).ToArray();
+
+            var handlers = new List<CollectionTransitHandler>(options.Length);
+            var operations = new List<Task>(options.Length);
+            
             // TODO pass number of runs into args
             while (token.IsCancellationRequested)
             {
-                var options = ConfigurationReader.Read(_configPath);
-                var operations = new List<Task>();
-                foreach (var option in options)
-                {
-                    operations.Add(CollectionTransitHandler.RunAsync(option, token));
-                }
 
+                for (var idx = 0; idx < options.Length; idx++)
+                {
+                    handlers[idx] ??= new CollectionTransitHandler(options[idx]);
+                    operations[idx] = handlers[idx].TransitAsync(token);
+                }
+                
                 await Task.WhenAll(operations);
             }        
         }
