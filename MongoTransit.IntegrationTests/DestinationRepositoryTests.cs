10f3a7108870008dad0a390f07270436898d860a
Alexey Maltsev <axel.user.perm@gmail.com>
2021-07-11 23:39:40 +0300

    Added tests for FindLastCheckpointAsync

--- MongoTransit.IntegrationTests/DestinationRepositoryTests.cs
+++ MongoTransit.IntegrationTests/DestinationRepositoryTests.cs
@@ -1,8 +1,16 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoTransit.IntegrationTests.Extensions;
 using MongoTransit.Storage.Destination;
 using Moq;
 using Serilog;
+using Xunit;
 
 namespace MongoTransit.IntegrationTests
 {
@@ -19,5 +27,56 @@ namespace MongoTransit.IntegrationTests
 
             _sut = new DestinationRepository(_destCollection, new Mock<ILogger>().Object);
         }
+
+        #region FindLastCheckpointAsync
+
+        [Fact]
+        public async Task FindLastCheckpointAsync_ShouldReturnNull_EmptyCollection()
+        {
+            // Act
+            var actual = await _sut.FindLastCheckpointAsync("Modified", CancellationToken.None);
+            
+            // Assert
+            actual.Should().BeNull();
+        }
+        
+        [Fact]
+        public async Task FindLastCheckpointAsync_ShouldReturnNull_NoCheckpointValueInCollection()
+        {
+            // Arrange
+            await _destCollection.InsertManyAsync(Enumerable.Range(0, 10)
+                .Select(_ => new BsonDocument
+                {
+                    ["Value"] = Fixture.Create<string>()
+                }));
+            
+            // Act
+            var actual = await _sut.FindLastCheckpointAsync("Modified", CancellationToken.None);
+            
+            // Assert
+            actual.Should().BeNull();
+        }
+        
+        [Fact]
+        public async Task FindLastCheckpointAsync_ShouldReturnMaxCheckpointValue_HasCheckpointValuesInCollection()
+        {
+            // Arrange
+            // Truncate checkpoint value due to differences in MongoDb and .Net resolution of DateTime, so
+            // assertion will be easier
+            var lastCheckpoint = DateTime.UtcNow.Truncate(TimeSpan.FromSeconds(1));
+            await _destCollection.InsertManyAsync(Enumerable.Range(0, 10)
+                .Select(offset => new BsonDocument
+                {
+                    ["Modified"] = new BsonDateTime(lastCheckpoint.AddMinutes(-offset))
+                }));
+            
+            // Act
+            var actual = await _sut.FindLastCheckpointAsync("Modified", CancellationToken.None);
+            
+            // Assert
+            actual.Should().Be(lastCheckpoint.AddMilliseconds(-lastCheckpoint.Millisecond));
+        }
+
+        #endregion
     }
 }
