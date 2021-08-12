bae6ee5afb6aed8695721956278c967406814982
Alexey Maltsev <axel.user.perm@gmail.com>
2021-08-12 23:32:46 +0300

    Made full restore test case

--- MongoTransit.IntegrationTests/TransitRunnerTests.cs
+++ MongoTransit.IntegrationTests/TransitRunnerTests.cs
@@ -11,51 +11,78 @@ using MongoTransit.Transit;
 using Serilog;
 using Serilog.Events;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace MongoTransit.IntegrationTests
 {
     public class TransitRunnerTests: IntegrationTestBase
     {
+        private readonly ITestOutputHelper _testOutputHelper;
+
         public record TestTransitOptions(string Database,
             string Collection,
             bool FetchKey,
             IterativeTransitOptions IterativeOptions);
 
-        public static IEnumerable<object[]> GetTestTransitOptions =>
+        public static IEnumerable<object[]> GetFullRestoreTestTransitOptions =>
             new List<object[]>
             {
                 new object[]
                 {
+                    "Single database with multiple collections (Full restore)",
                     new TestTransitOptions[]
                     {
-                        new("Test", "Test", true, null)
+                        new("TestDb", "Test1", true, null),
+                        new("TestDb", "Test2", true, null),
+                        new("TestDb", "Test3", true, null)
+                    }
+                },
+                new object[]
+                {
+                    "Multiple databases with multiple collections (Full restore)",
+                    new TestTransitOptions[]
+                    {
+                        new("TestDb1", "Test1", true, null),
+                        new("TestDb1", "Test2", true, null),
+                        new("TestDb2", "Test1", true, null),
+                        new("TestDb2", "Test2", true, null),
                     }
                 }
             };
         
         
-        // TODO: clean-up collections
         private readonly HashSet<(string Database, string Collection)> _createdCollections = new();
 
+        public TransitRunnerTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Theory]
-        [MemberData(nameof(GetTestTransitOptions))]
-        public async Task RunAsync_ShouldTransferAllData_DestinationCollectionIsSharded(TestTransitOptions[] options)
+        [MemberData(nameof(GetFullRestoreTestTransitOptions))]
+        public async Task RunAsync_ShouldTransferAllData_DestinationCollectionIsSharded_FullRestore(string testCase, TestTransitOptions[] options)
         {
             // Arrange
             const int documentsCount = 1_000;
+            
+            _testOutputHelper.WriteLine($"Test case: {testCase}");
+            var dbNames = new Dictionary<string, string>();
 
             foreach (var option in options)
             {
-                await CreateCollectionAsync(option.Database, option.Collection);
+                dbNames.TryAdd(option.Database, $"{option.Database}-{Guid.NewGuid()}");
+                var db = dbNames[option.Database];
+                
+                await CreateCollectionAsync(db, option.Collection);
                 var entities = Fixture.CreateMany<Entity>(documentsCount);
-                await SourceClient.GetDatabase(option.Database).GetCollection<Entity>(option.Collection)
+                await SourceClient.GetDatabase(db).GetCollection<Entity>(option.Collection)
                     .InsertManyAsync(entities);
             }
 
             var transitOptions = options.Select(o => new CollectionTransitOptions(SourceConnectionString,
                 DestinationConnectionString,
-                o.Database, o.Collection, new[] { nameof(Entity.ShardedKey) }, o.FetchKey, 4, 100, true,
-                o.IterativeOptions)).ToArray(); 
+                dbNames[o.Database], o.Collection, new[] { nameof(Entity.ShardedKey) }, o.FetchKey, 4, 100, true,
+                null)).ToArray(); 
             
             // Act
             await TransitRunner.RunAsync(CreateLogger(), transitOptions, SingeCycle(), false,
@@ -64,10 +91,11 @@ namespace MongoTransit.IntegrationTests
             // Assert
             foreach (var option in options)
             {
-                var count = await DestinationClient.GetDatabase(option.Database).GetCollection<Entity>(option.Collection)
+                var db = dbNames[option.Database];
+                var count = await DestinationClient.GetDatabase(db).GetCollection<Entity>(option.Collection)
                     .CountDocumentsAsync(FilterDefinition<Entity>.Empty);
                 count.Should().Be(documentsCount,
-                    $"Collection {option.Database}.{option.Collection} should have all documents");
+                    $"Collection {db}.{option.Collection} should have all documents");
             }
         }
 
