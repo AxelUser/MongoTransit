b83d83fcee176ab514080721cbef2af7cd25e26d
Alexey Maltsev <axel.user.perm@gmail.com>
2021-08-14 21:46:14 +0300

    Implemented AddShardToZoneAsync and UpdateZoneKeyRangeAsync methods at MongoHelper

--- MongoTransit.IntegrationTests/Helpers/MongoHelper.cs
+++ MongoTransit.IntegrationTests/Helpers/MongoHelper.cs
@@ -22,13 +22,39 @@ namespace MongoTransit.IntegrationTests.Helpers
             VerifyOk(await _adminDb.RunCommandAsync<BsonDocument>(new BsonDocument
             {
                 ["enableSharding"] = databaseName
-            }), "Enable Database Sharding");
+            }), $"Enable database '{databaseName}' sharding");
 
             VerifyOk(await _adminDb.RunCommandAsync<BsonDocument>(new BsonDocument
             {
                 ["shardCollection"] = $"{databaseName}.{collectionName}",
                 ["key"] = new BsonDocument(key, 1)
-            }), "Sharding Collection");
+            }), $"Sharding {databaseName}.{collectionName} collection by key {key}");
+        }
+
+        public async Task AddShardToZoneAsync(string shardName, string zoneName)
+        {
+            VerifyOk(await _adminDb.RunCommandAsync<BsonDocument>(new BsonDocument
+            {
+                ["addShardToZone"] = shardName,
+                ["zone"] = zoneName
+            }), $"Adding shard {shardName} to zone {zoneName}");
+        }
+
+        public async Task UpdateZoneKeyRangeAsync(string databaseName,
+            string collectionName,
+            string key,
+            string min,
+            string max,
+            string zoneName)
+        {
+            VerifyOk(await _adminDb.RunCommandAsync<BsonDocument>(new BsonDocument
+                {
+                    ["updateZoneKeyRange"] = $"{databaseName}.{collectionName}",
+                    ["min"] = new BsonDocument(key, min),
+                    ["max"] = new BsonDocument(key, max),
+                    ["zone"] = zoneName
+                }),
+                $"Updating zone {zoneName} of collection {databaseName}.{collectionName} with key {key} ranges from {min} to {max}");
         }
 
         private void VerifyOk(BsonDocument operationResult, string operationName)
