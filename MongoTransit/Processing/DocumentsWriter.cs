994e96f91dfc59be7bc45f3aba184abaafdc9802
Alexey Maltsev <axel.user.perm@gmail.com>
2021-08-17 23:48:07 +0300

    Made DocumentsWriter stateless

--- MongoTransit/Processing/DocumentsWriter.cs
+++ MongoTransit/Processing/DocumentsWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Channels;
@@ -13,9 +14,8 @@ namespace MongoTransit.Processing
     public class DocumentsWriter : IDocumentsWriter
     {
         private readonly ILogger _logger;
-        private readonly Channel<ReplaceOneModel<BsonDocument>> _retriesChannel;
-        private readonly Task<WorkerResult>[] _insertionWorkers;
-        private readonly Task<WorkerResult>[] _retryWorkers;
+        private readonly int _insertionWorkersCount;
+        private readonly int _retryWorkersCount;
         private readonly string _collectionName;
         private readonly IWriteWorkerFactory _writeWorkerFactory;
 
@@ -28,60 +28,53 @@ namespace MongoTransit.Processing
             _collectionName = collectionName;
             _writeWorkerFactory = writeWorkerFactory;
             _logger = logger;
-            _insertionWorkers = insertionWorkersCount > 0
-                ? new Task<WorkerResult>[insertionWorkersCount]
+            _insertionWorkersCount = insertionWorkersCount > 0
+                ? insertionWorkersCount
                 : throw new ArgumentOutOfRangeException(nameof(insertionWorkersCount), insertionWorkersCount, "Should be greater than zero");
-            _retryWorkers = retryWorkersCount >= 0
-                ? new Task<WorkerResult>[retryWorkersCount]
+            _retryWorkersCount = retryWorkersCount >= 0
+                ? retryWorkersCount
                 : throw new ArgumentOutOfRangeException(nameof(insertionWorkersCount), insertionWorkersCount, "Should be positive");
-            
-            _retriesChannel = Channel.CreateUnbounded<ReplaceOneModel<BsonDocument>>();
         }
 
         public async Task<TransferResults> WriteAsync(CancellationToken token)
         {
-            StartWorkers(token);
-            return await WaitUntilFinishedAsync();
-        }
-
-        private void StartWorkers(CancellationToken token)
-        {
-            for (var workerN = 0; workerN < _insertionWorkers.Length; workerN++)
+            var insertionWorkers = new Task<WorkerResult>[_insertionWorkersCount];   
+            var retryWorkers = new Task<WorkerResult>[_retryWorkersCount];
+            var retriesChannel = Channel.CreateUnbounded<ReplaceOneModel<BsonDocument>>();
+            
+            for (var workerN = 0; workerN < insertionWorkers.Length; workerN++)
             {
                 var workerLogger = _logger.ForContext("Scope", $"{_collectionName}-{workerN:00}");
-                _insertionWorkers[workerN] = _writeWorkerFactory.RunWorker(_retriesChannel.Writer,
+                insertionWorkers[workerN] = _writeWorkerFactory.RunWorker(retriesChannel.Writer,
                     workerLogger, token);
             }
 
-            for (var retryWorkerN = 0; retryWorkerN < _retryWorkers.Length; retryWorkerN++)
+            for (var retryWorkerN = 0; retryWorkerN < retryWorkers.Length; retryWorkerN++)
             {
                 var retryLogger = _logger.ForContext("Scope", $"{_collectionName}-Retry{retryWorkerN:00}");
-                _retryWorkers[retryWorkerN] = _writeWorkerFactory.RunRetryWorker(_retriesChannel.Reader,
+                retryWorkers[retryWorkerN] = _writeWorkerFactory.RunRetryWorker(retriesChannel.Reader,
                     retryLogger, token);
             }
 
-            _logger.Debug("Started {I:N0} insertion worker(s) and {R:0} retry worker(s)", _insertionWorkers.Length, _retryWorkers.Length);
-        }
-
-        private async Task<TransferResults> WaitUntilFinishedAsync()
-        {
+            _logger.Debug("Started {I:N0} insertion worker(s) and {R:0} retry worker(s)", insertionWorkers.Length, retryWorkers.Length);
+            
             _logger.Debug("Waiting for insertion-workers to finish");
-            await Task.WhenAll(_insertionWorkers);
+            await Task.WhenAll(insertionWorkers);
             
             _logger.Debug("Insertion-workers finished, waiting for retry-workers");
-            _retriesChannel.Writer.Complete();
-            await Task.WhenAll(_retryWorkers);
-
-            return await GetResultsAsync();
+            retriesChannel.Writer.Complete();
+            await Task.WhenAll(retryWorkers);
+            
+            return await GetResultsAsync(insertionWorkers.Concat(retryWorkers));
         }
 
-        private async Task<TransferResults> GetResultsAsync()
+        private static async Task<TransferResults> GetResultsAsync(IEnumerable<Task<WorkerResult>> finishedWorkers)
         {
             var processed = 0L;
             var retried = 0L;
             var failed = 0L;
 
-            foreach (var worker in _insertionWorkers.Concat(_retryWorkers))
+            foreach (var worker in finishedWorkers)
             {
                 var (s, r, f) = await worker;
                 processed += s;
