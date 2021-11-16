ffca8cac9f19388e929402965749846f603b6157
Alexey Maltsev <axel.user.perm@gmail.com>
2021-11-16 23:50:06 +0300

    Added ConfigurationValidator

---
+++ MongoTransit/Configuration/ConfigurationValidator.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MongoTransit.Configuration;
+
+public static class ConfigurationValidator
+{
+    public static IEnumerable<string> Validate(ConfigurationReader.CollectionOption[] collectionOptions)
+    {
+        for (var i = 0; i < collectionOptions.Length; i++)
+        {
+            var option = collectionOptions[i];
+            if (string.IsNullOrWhiteSpace(option.Database))
+                yield return $"[{i}] {nameof(option.Database)} should not be empty";
+
+            if (string.IsNullOrWhiteSpace(option.Name))
+                yield return $"[{i}] {nameof(option.Name)} should not be empty";
+
+            if (option.KeyFields?.Any(string.IsNullOrWhiteSpace) == true)
+                yield return $"[{i}] {nameof(option.KeyFields)} should not contain empty values";
+            
+            if (option.IterativeOptions == null)
+                continue;
+
+            var iter = option.IterativeOptions;
+
+            if (string.IsNullOrWhiteSpace(iter.Field))
+                yield return $"[{i}.{nameof(option.IterativeOptions)}] {nameof(iter.Field)} should not be empty";
+
+            if (iter.OffsetInMinutes < 0)
+                yield return
+                    $"[{i}.{nameof(option.IterativeOptions)}] {nameof(iter.OffsetInMinutes)} should greater or equal to zero (current value is {iter.OffsetInMinutes})";
+        }
+    }
+}
